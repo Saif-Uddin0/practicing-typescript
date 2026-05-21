ed6b5dfbe821ec99d373c8f3f2f8a2d0e13b8768
Saif Uddin <mahirr.846@gmail.com>
2026-05-21 19:50:34 +0600

    learn the primary oop

--- Module-3/class.ts
+++ Module-3/class.ts
@@ -1,14 +1,23 @@
 class Animal {
-    name: string;
-    species: string;
-    sound: string;
+    // name: string;
+    // species: string;
+    // sound: string;
 
 
-    constructor(name:string , species: string , sound: string){
+    constructor(public name: string, public species: string, public sound: string) {
         this.name = name;
         this.species = species;
-        this.sound = sound
+        this.sound = sound;
+
     }
+
+    makeSound() {
+        return(`${this.name} is making sound ${this.sound}`);
+
+    }
+
 }
 
-const dog = new Animal("dogesh bhai", "dog" , "Ghew ghew")
+const dog = new Animal("dogesh bhai", "dog", "Ghew ghew");
+const cat = new Animal("cat bhai", "cat", "mew mew");
+console.log(cat.makeSound());
