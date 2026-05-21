059d6e8a93ba332579d3b34e464dcf2e05081882
Saif Uddin <mahirr.846@gmail.com>
2026-05-21 19:29:52 +0600

    learnign the class and object

---
+++ Module-3/class.ts
@@ -0,0 +1,14 @@
+class Animal {
+    name: string;
+    species: string;
+    sound: string;
+
+
+    constructor(name:string , species: string , sound: string){
+        this.name = name;
+        this.species = species;
+        this.sound = sound
+    }
+}
+
+const dog = new Animal("dogesh bhai", "dog" , "Ghew ghew")
