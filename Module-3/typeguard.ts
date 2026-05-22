b6eaafd454142d4ec015f1ea5507b8428b888a9d
Saif Uddin <mahirr.846@gmail.com>
2026-05-22 15:14:13 +0600

    learing the type guard

---
+++ Module-3/typeguard.ts
@@ -0,0 +1,29 @@
+//  Type Guard && in typeof
+
+type AlphaNumranic = number | string
+
+const add = (num1: AlphaNumranic, num2: AlphaNumranic) => {
+
+    if(typeof num1 === "number" && typeof num2 === "number"){
+        return num1+ num2
+    }
+    else{
+        num1.toLocaleString() + num2.toLocaleString()
+    }
+
+}
+
+
+
+// in guard
+
+type NormalUser ={
+    name : string;
+
+}
+type AdminUser ={
+    name: string;
+    role: "Admin"
+}
+
+const userInfo = 
