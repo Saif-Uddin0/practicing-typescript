85caad57ba4b2ec0945c95ea2af0910ffb72cb57
Saif Uddin <mahirr.846@gmail.com>
2026-05-22 14:59:52 +0600

    basic understanding of inheritance

--- Module-3/inheritance.ts
+++ Module-3/inheritance.ts
@@ -1,24 +1,70 @@
-class student{
-    name: string;
-    age: number;
-    address: string;
+class Parent{
+    name: string; //common
+    age: number; //common
+    address: string;  //common
 
 
-    constructor(name: string,age: number,address: string){
-        this.name =name;
+    constructor(name: string, age: number, address: string){
+        this.name = name;
         this.age = age;
         this.address = address;
     }
 
 
+    //common
     getSleep(numberOfHours : number){
         console.log(`${this.name} ${numberOfHours} ghonta ghumay`);
-        
+
     }
 }
 
 
 
-const student_1 = new student("Mr. fakibaaz" , 18 ,"Mohakhali-Dhaka");
 
-student_1.getSleep(1)
+
+
+
+
+
+class student extends Parent{}
+
+
+
+const student_1 = new student("Mr. fakibaaz", 18, "Mohakhali-Dhaka");
+
+student_1.getSleep(15)
+
+
+
+
+
+
+
+
+// teacher
+class Teacher extends Parent {
+    designation: string; //own property
+
+
+    constructor(name: string, age: number, address: string ,designation: string) {
+
+
+        super(name ,age , address)
+        this.designation = designation;
+    }
+
+
+    //common method
+
+
+    //own method 
+    takeClass(numberOfClass: number) {
+        console.log(`${this.name} ${numberOfClass} ghonta class ney`);
+
+    }
+}
+
+
+const teacher1 = new Teacher("Mr. Mofiz", 46, "Dhaka", "Frontend Dev.")
+
+teacher1.takeClass(4)
