43aefe39dc18d43bab35c3f28dac31132c728406
Saif Uddin <mahirr.846@gmail.com>
2026-05-26 01:20:21 +0600

    knowing some of the abstraction

--- Module-3/abstraction.ts
+++ Module-3/abstraction.ts
@@ -2,31 +2,60 @@
 
 
 // interface
+//abstract class
 
-interface MediaPlayer{
-    play(): void;
-    pause() : void;
-    stop() : void;
-}
+
+// idea
+// interface MediaPlayer{
+//     play(): void;
+//     pause() : void;
+//     stop() : void;
+// }
 
 
 
 // implementations
-class MusicPlayer implements MediaPlayer{
-    play(): void {
-        console.log("playing music....");
+// class MusicPlayer implements MediaPlayer{
+//     play(): void {
+//         console.log("playing music....");
         
-    }
-    pause(): void {
-        console.log("playing pause....");
+//     }
+//     pause(): void {
+//         console.log("playing pause....");
         
-    }
-    stop(): void {
-        console.log("playing stop....");
+//     }
+//     stop(): void {
+//         console.log("playing stop....");
         
-    }
+//     }
+// }
+
+
+// const MezbaPlayer = new MusicPlayer();
+// MezbaPlayer.play();
+
+
+//idea
+abstract class MediaPlayer {
+   abstract play(): void;
+    abstract pause() : void;
+    abstract stop() : void;
 }
 
 
-const MezbaPlayer = new MusicPlayer();
-MezbaPlayer.play();
+// implementation
+class MezbaPlayer extends MediaPlayer{
+    play(): void {
+        console.log("playing music...");
+        
+    }
+    pause(): void {
+        console.log("pausing music...");
+        
+    }
+    stop(): void {
+        console.log("stopping music...");
+        
+    }
+}
+const mezbapler1 = new MezbaPlayer()
